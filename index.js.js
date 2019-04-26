f65c235fec1db9af02adbf3244a7dd9b5363b041
stroooooooooooooooooooooong <49954731+stroooooooooooooooooooooong@users.noreply.github.com>
2019-04-26 20:24:09 +0300

    Add files via upload

---
+++ index.js.js
@@ -0,0 +1,41 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+const developers = ('517694113190117386')
+const adminprefix = ('!');
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'sp')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**Status You   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'sw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**Status You   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'sl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**Status You  ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/gamer");
+      message.channel.send(`**Status You ${argresult} **`)
+    } else 
+  
+  if (message.content.startsWith(! + 'sn')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(! + 'sa')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+client.login(process.env.BOT_TOKEN);
